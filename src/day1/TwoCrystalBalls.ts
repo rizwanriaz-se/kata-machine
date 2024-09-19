6ba7612d93380c9941af31068b796b3e5472b37e
Rizwan Riaz <iamrizwan077@gmail.com>
2024-09-19 13:10:14 +0500

    Two Crystal Balls Problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,38 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jump = Math.floor(Math.sqrt(breaks.length));
+    let i;
+    for (i = 0; i <= breaks.length; i += jump) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= jump;
 
+    for (let j = i; j <= i + jump; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+    return -1;
 }
+
+// Solution by instructor
+// export default function two_crystal_balls(breaks: boolean[]): number {
+//     const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+
+//     let i = jmpAmount;
+//     for (; i <= breaks.length; i += jmpAmount) {
+//         if (breaks[i]) {
+//             break;
+//         }
+//     }
+
+//     i -= jmpAmount;
+
+//     for (let j = 0; j < jmpAmount && i < breaks.length; ++j, ++i) {
+//         if (breaks[i]) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
